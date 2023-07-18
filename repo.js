2960601fe429f8bab07080ac4521e4a9f5f3b0c8
Jan Hořava <czechinthemail@gmail.com>
2023-07-18 13:49:01 -0700

    combined js into repo.js

--- repo.js
+++ repo.js
@@ -145,3 +145,76 @@ function insert(text){
     calculator.selectionEnd = position + text.length;
     calculator.focus();
 }
+
+function repo_init(){
+    core_repo_init({
+      'events': {
+        'calculate': {
+          'onclick': calculate,
+        },
+        'calculate-interest': {
+          'onclick': calculate_interest,
+        },
+        'calculate-percent': {
+          'onclick': calculate_percent,
+        },
+        'clear': {
+          'onclick': function(){
+              const calculator = document.getElementById('calculator');
+              calculator.value = '';
+              calculator.focus();
+          },
+        },
+        'height': {
+          'oninput': calculate_width,
+        },
+        'pi': {
+          'onclick': function(){
+              insert('π');
+          },
+        },
+        'ratio-height': {
+          'oninput': calculate_width,
+        },
+        'ratio-width': {
+          'oninput': calculate_height,
+        },
+        'width': {
+          'oninput': calculate_height,
+        },
+      },
+      'keybinds': {
+        13: {
+          'todo': function(event){
+              const calculator = document.getElementById('calculator');
+
+              if(calculator === document.activeElement){
+                  event.preventDefault();
+                  if(event.shiftKey){
+                      insert('\n');
+                      return;
+                  }
+              }
+
+              calculate();
+          },
+        },
+      },
+      'storage': {
+        'calculator': '',
+        'compound': false,
+        'decimals-min': 2,
+        'interest': 0,
+        'principal': 0,
+        'step-interval': 1,
+        'step-limit': 100,
+        'step-max': 10,
+        'step-start': 0,
+        'time': 0,
+      },
+      'title': 'Math.htm',
+    });
+
+    core_storage_update();
+    calculate();
+}
